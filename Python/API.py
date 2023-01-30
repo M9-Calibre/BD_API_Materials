140ee0a979908005510ba79e3049fc05eb1d066d
Afonso Campos <afonso.campos@ua.pt>
2023-01-30 09:22:38 +0000

    Update python files

--- Python/API.py
+++ Python/API.py
@@ -22,24 +22,63 @@ def get_user_list(username, password):
 # Can only be performed by a superuser
 # Steps to create superuser in the README.md
 def create_upper_category(username, password, category_name):
-    return requests.post(f"{URL}/categories/upper/", auth=(username,password), json={"category":category_name})
+    return requests.post(f"{URL}/categories/upper", auth=(username,password), json={"category":category_name})
 
 # Can only be performed by a superuser
 # An upper category must already be created to link to
 def create_middle_category(username, password, upper_category, category_name):
-    return requests.post(f"{URL}/categories/middle/", auth=(username,password), json={"category":category_name,"upper_category":upper_category})
+    upper_category_id = get_upper_category_id(username, password, upper_category)
+    return requests.post(f"{URL}/categories/middle", auth=(username,password), json={"category":category_name,"upper_category":upper_category_id})
 
 # Can only be performed by a superuser
 # A middle category must already be created to link to
-def create_lower_category(username, password, upper_category, category_name):
-    return requests.post(f"{URL}/categories/lower/", auth=(username,password), json={"category":category_name,"upper_category":upper_category})
+def create_lower_category(username, password, upper_category, middle_category, category_name):
+    middle_category_id = get_middle_category_id(username, password, upper_category, middle_category)
+    return requests.post(f"{URL}/categories/lower", auth=(username,password), json={"category":category_name,"upper_category":middle_category_id})
 
-def get_category_tree():
-    return requests.get(f"{URL}/categories/upper/")
+def get_category_tree(username, password):
+    return requests.get(f"{URL}/categories/upper?format=json", auth=(username,password)).json()['results']
+
+def get_upper_category_id(username, password, upper_category):
+    results = requests.get(f"{URL}/categories/upper?upper_category={upper_category}&format=json", 
+                            auth=(username,password)).json()['results']
+    if len(results) != 1:
+        return None
+    return results[0]['id']
+
+def get_middle_category_id(username, password, upper_category, middle_category):
+    results = requests.get(f"{URL}/categories/middle?upper_category={upper_category}&middle_category={middle_category}&format=json", 
+                            auth=(username,password)).json()['results']
+    if len(results) != 1:
+        return None
+    return results[0]['id']
+
+def get_lower_category_id(username, password, upper_category, middle_category, category):
+    results = requests.get(f"{URL}/categories/lower?upper_category={upper_category}&middle_category={middle_category}&category={category}&format=json",
+                            auth=(username,password)).json()['results']
+    if len(results) != 1:
+        return None
+    return results[0]['id']
 
 # Example material in material.json
 # Can only be performed by an authenticated user
 # "category" parameter must be the id (primary key) of a lower category object (which can be checked at GET /categories/upper/)
-def create_material(username, password, material_json):
-    return requests.post(f"{URL}/materials/", auth=(username,password), json=material_json)
+def create_material(username, password, material_json : dict, upper_cat="Other", middle_cat="Other", cat="Other"):
+    if not material_json.get("category"):
+        lower_category_id = get_lower_category_id(username, password, upper_cat, middle_cat, cat)
+        material_json["category"] = lower_category_id
+    return requests.post(f"{URL}/materials/?format=json", auth=(username,password), json=material_json)
+
+def get_material_list():
+    return requests.get(f"{URL}/materials/?format=json").json()['results']
+
+# Any authenticated user, does not need to be material owner
+def create_test(username, password, test_name, material_id, DIC_params : dict, thermog_params : dict):
+    test_json = {
+        "material": material_id,
+        "name": test_name,
+        "DIC_params": DIC_params,
+        "thermog_params": thermog_params
+    }
+    return requests.post(f"{URL}/tests/?format=json", auth=(username,password), json=test_json)
 
--- Python/PopulateAPI.py
+++ Python/PopulateAPI.py
@@ -1,6 +1,7 @@
 from API import *
+import json
 
-# superuser must be created as indicated in README.md
+# superuser must be created as indicated in README.md, change variables to your liking
 username_staff = "afonso"
 password_staff = "1234"
 
@@ -26,7 +27,8 @@ print(f"Register Bob: {x.status_code}")
 
 # Get user list (should have 3 users)
 
-print(get_user_list(username_staff,password_staff)['results'])
+user_list = get_user_list(username_staff,password_staff)
+print(json.dumps(user_list, indent=3))
 
 # Create come categories as superuser
 
@@ -44,5 +46,83 @@ print(f"Create upper category Other: {x.status_code}")
 x = create_middle_category(username_staff, password_staff, "Metal", "Steel")
 print(f"Create middle category Metal->Steel: {x.status_code}")
 
+x = create_middle_category(username_staff, password_staff, "Metal", "Aluminium")
+print(f"Create middle category Metal->Aluminium: {x.status_code}")
 
+x = create_middle_category(username_staff, password_staff, "Metal", "Other")
+print(f"Create middle category Metal->Other: {x.status_code}")
 
+x = create_middle_category(username_staff, password_staff, "Glass", "Laminated Glass")
+print(f"Create middle category Glass->Laminated Glass: {x.status_code}")
+
+x = create_middle_category(username_staff, password_staff, "Glass", "Other")
+print(f"Create middle category Glass->Other: {x.status_code}")
+
+x = create_middle_category(username_staff, password_staff, "Other", "Other")
+print(f"Create middle category Other->Other: {x.status_code}")
+
+# Lower
+x = create_lower_category(username_staff, password_staff, "Metal", "Steel", "Alloy Steel")
+print(f"Create lower category Metal->Steel->Alloy Steel: {x.status_code}")
+
+x = create_lower_category(username_staff, password_staff, "Metal", "Aluminium", "Aluminium 1100")
+print(f"Create lower category Metal->Aluminium->Aluminium 1100: {x.status_code}")
+
+x = create_lower_category(username_staff, password_staff, "Glass", "Laminated Glass", "EVA")
+print(f"Create lower category Glass->Laminated Glass->EVA: {x.status_code}")
+
+x = create_lower_category(username_staff, password_staff, "Other", "Other", "Other")
+print(f"Create middle category Other->Other->Other: {x.status_code}")
+
+# Check category trees
+
+category_tree = get_category_tree(username_staff, password_staff)
+print(json.dumps(category_tree, indent=3))
+
+# Create some materials
+
+with open("material.json", "rb") as f:
+    material1 = json.load(f)
+
+with open("material2.json", "rb") as f:
+    material2 = json.load(f)
+
+# Teresa submits a material
+x = create_material(username_teresa, password_teresa, material1, upper_cat="Metal", middle_cat="Steel", cat="Alloy Steel")
+print(f"Teresa creates a material: {x.status_code}")
+material1_id = x.json()['id']
+print(f"Material -> ID: {material1_id}, Owner: {x.json()['submitted_by']}")
+
+# Bob submits a material
+x = create_material(username_bob, password_bob, material2, upper_cat="Glass", middle_cat="Laminated Glass", cat="EVA")
+print(f"Bob creates a material: {x.status_code}")
+material2_id = x.json()['id']
+print(f"Material -> ID: {material2_id}, Owner: {x.json()['submitted_by']}")
+
+# Bob attempts to submit a material with the same name as Teresa's material
+# This should fail
+x = create_material(username_bob, password_bob, material1, upper_cat="Glass", middle_cat="Laminated Glass", cat="EVA")
+print(f"Bob attempts to create a material (should fail): {x.status_code}, {x.reason}")
+
+materials = get_material_list()
+print(f"Number of materials: {len(materials)}")
+
+# Test Creation
+# Bob creates test for Teresas Alloy Steel material
+
+dic_params_example = {
+    "Camera": "FlirBackflyBFS-U3-51S5M-C",
+    "Focal length": 12.5,
+    "Image resolution": "2448x2048 px2",
+    "Camera noise": "0.48% ofrange",
+    "Working distance": "251 nm",
+    "Image conversion factor": "0.05039 mm/px",
+    "Average speckle size": "3px",
+    "Subject size": "21 px",
+    "Step size": "5px"
+}
+ 
+x = create_test(username_bob, password_bob, "Test A", material1_id, dic_params_example, None)
+print(f"Bob submits a test: {x.status_code}, {x.reason}, {x.content}")
+test_id = x.json()['id']
+print(f"Test -> ID: {test_id}, Owner: {x.json()['submitted_by']}, Material: {x.json()['material']}")
