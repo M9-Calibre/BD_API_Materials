bf9b8b5bfc3374c0cc3e4ad6723127b2540cf120
mtcdultra <marcostuliobr@gmail.com>
2023-02-07 15:40:25 +0000

    updated db configuration

--- API/djangoProject/settings.py
+++ API/djangoProject/settings.py
@@ -94,21 +94,23 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dem-vx',
-        'USER': 'dem-vxs-dbo',
-        'PASSWORD': get_secret("DB_KEY"),
-        'HOST': 'mysql-hosting.ua.pt',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'dem-vx',
+    #     'USER': 'dem-vxs-dbo',
+    #     'PASSWORD': get_secret("DB_KEY"),
+    #     'HOST': 'mysql-hosting.ua.pt',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #     }
+    # }
 }
 
 
