8b2321a100c8a35499208099b3f3cf0eee19742c
AfonsoCampos971 <afonso.campos@ua.pt>
2023-06-12 14:35:05 +0100

    Give admin all permissions

--- API/API_Materials/permissions.py
+++ API/API_Materials/permissions.py
@@ -5,6 +5,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if request.user.is_staff:
+            return True
         return obj.submitted_by == request.user
 
 
